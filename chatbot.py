0d1b8de56b7390db09136217097f020a3e12d334
amilcar-aga <agonzalezalasia@gmail.com>
2026-02-16 22:51:51 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,45 @@
 import discord
 import random
 from discord.ext import commands
+import os
+import requests
 from app import get_password 
-
+from numero_al_azar import numero_random
 permisos = discord.Intents.default()
 permisos.message_content = True
 
 harvis = commands.Bot(command_prefix="/", intents=permisos)
 
 
+#------------------------------VARIABLES-------------------------------#
+juego_activo = False 
+numero_secreto = None 
+intentos = 5
 
+#------------------------------API´s-----------------------------------#
+def get_dog_image_url():    
+    url = "https://random.dog/woof.json"
+    res = requests.get(url)
+    data = res.json()
+    return data["url"]
+
+def get_fox_image_url():    
+    url = "https://randomfox.ca/floof/"
+    res = requests.get(url)
+    data = res.json()
+    return data["image"]
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_tokyo_image_url():    
+    url = "https://kitsu.io/api/edge/anime?filter[text]=tokyo"
+    res = requests.get(url)
+    data = res.json()
+    return data["links"]
 #-------------------------------EVENTOS--------------------------------#
 @harvis.event
 async def on_ready():
@@ -35,12 +65,51 @@ async def moneda(ctx):
 @harvis.command()
 async def emoji(ctx):
     emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
-    return random.choice(emodji)
+    await ctx.send(f"{random.choice(emodji)}")
 
-
+@harvis.command()
+async def play(ctx, numero_del_usuario:int = None):
+    global juego_activo, numero_secreto, intentos
+    if not juego_activo:
+        mensaje, numero_secreto, intentos, juego_activo = numero_random()
+        await ctx.send(f"{mensaje}")
+        return
+    if numero_del_usuario == None:
+        mensaje, numero_secreto, intentos, juego_activo = numero_random()
+        await ctx.send(f"{mensaje}")
+        return
+    
 
 @harvis.command()
 async def comandos(ctx):
-    await ctx.send(f"mis comandos son: /moneda, /password, /hola, /comandos, /emoji")
+    await ctx.send(f"mis comandos son: /moneda, /password, /hola, /comandos, /emoji, /play(comando experimental), /meme /dogs, /foxs, /duck, /tokyo(comando experimental)")
+
+@harvis.command()
+async def meme(ctx):
+    lista_imagenes = os.listdir("img")
+    imagen_enviar = random.choice(lista_imagenes)
+    with open(f"img/{imagen_enviar}", "rb") as f:
+        image = discord.File(f)
+        await ctx.send(file = image)
+
+@harvis.command('dogs')
+async def dogs(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+@harvis.command('foxs')
+async def foxs(ctx):
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
+
+@harvis.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@harvis.command('tokyo')
+async def tokyo(ctx):
+    image_url = get_tokyo_image_url()
+    await ctx.send(image_url)
 
 harvis.run("token")
